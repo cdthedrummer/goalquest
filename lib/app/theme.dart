e003a9bb8699c4a6dfe049a73efd9ac7c09da043
cdthedrummer <113496461+cdthedrummer@users.noreply.github.com>
2025-01-26 12:50:05 -0500

    Add theme configuration

---
+++ lib/app/theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.deepPurple,
+      brightness: Brightness.light,
+    ),
+    cardTheme: const CardTheme(
+      margin: EdgeInsets.all(8.0),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      filled: true,
+    ),
+  );
+
+  static final darkTheme = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.deepPurple,
+      brightness: Brightness.dark,
+    ),
+    cardTheme: const CardTheme(
+      margin: EdgeInsets.all(8.0),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      filled: true,
+    ),
+  );
+}
